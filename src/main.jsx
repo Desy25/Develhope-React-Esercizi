48ebbd0001fe28079c1b0f3367d96e93b6e9de30
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-06 01:00:38 +0200

    ex8

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { Hello } from "./Hello";
+import { App } from "./App";
 import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(<Hello />)
+createRoot(document.getElementById('root')).render(<App />)
