ac2f3f9f6b54124fb8a54946872f6d8454d0f2ed
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-05 21:26:30 +0200

    ex3

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,7 @@ export function App() {
     function helloName(name) {
         return <h1>Hello, {name}</h1>
     }
-    return helloName("Jules");
+    return helloName();
 }
+
+// Se non passo la variabile semplicemente mi ritornerà la stringa "Hello, ", perchè il parametro non è stato definito.
