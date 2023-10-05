6fcb4c1ecb1470a90602a53b670e4430ceb93127
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-06 01:46:45 +0200

    ex15

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  }
 })
+
+// VITE v4.4.10  ready in 181 ms
+
+// ➜  Local:   http://localhost:3000/
+// ➜  Network: use --host to expose
+// ➜  press h to show help
