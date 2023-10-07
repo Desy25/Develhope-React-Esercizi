ec96a98d15b688e0566fb1fa91c384da8efc01e5
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-08 00:10:05 +0200

    ex16

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { Hello } from "./Hello";
-import { Message } from "./Message";
+
+import { Welcome } from "./Welcome";
 
 export function App() {
     return <div>
-        <Hello />
-        <Message />
+       <Welcome name="Violante" />
     </div>
 }
