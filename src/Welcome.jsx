eefd6f46e4cb3b1a5954f80feceef9319c885b70
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-08 02:04:06 +0200

    ex23

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -3,6 +3,6 @@ import { Age } from "./Age";
 export function Welcome({ name, welcomeAge }) {
     return <div>
         <p>Welcome, {name}!</p>
-        {welcomeAge && <Age age={welcomeAge} />}
+        {(welcomeAge > 18 && welcomeAge < 65)  && <Age age={welcomeAge} />}
     </div>
 }
