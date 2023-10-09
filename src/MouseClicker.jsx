fbd3a1696f60d40b0485c44623a796111df3eadd
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-09 02:41:58 +0200

    ex37

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -2,5 +2,5 @@ export function MouseClicker() {
     function handleButton(event) {
         console.log(event.currentTarget.name)
     }
-    return <button name="one" onClick={handleButton}>Click here</button>
+    return <button name="one" onClick={handleButton}><img height={20} width={20} />Click here</button>
 }
