737363068121aad2fd253a44b3c76c58b46a431a
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-09 02:50:27 +0200

    ex38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -2,5 +2,10 @@ export function MouseClicker() {
     function handleButton(event) {
         console.log(event.currentTarget.name)
     }
-    return <button name="one" onClick={handleButton}><img height={20} width={20} />Click here</button>
+    function handleEvent(event) {
+        console.log(event.currentTarget.src);
+    }
+    return <button name="one" onClick={handleButton}><img src="Button-Image" height={20} width={20} onClick={handleEvent} />Click here</button>
 }
+
+//Quando il "click" verrà eseguito sul tag img verranno comunque entrambi visualizzati in console per via dell'effetto "bubble" di html perchè img si trova all'interno del button.
