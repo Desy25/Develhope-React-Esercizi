33487e894c3538967041bd7468832eb16e4578aa
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-11 23:35:52 +0200

    ex51

--- src/FocusableInput.jsx
+++ src/FocusableInput.jsx
@@ -3,7 +3,13 @@ import { useEffect, useRef } from "react"
 export function FocusableInput() {
     const ref = useRef(null);
 
+    const mountedRef = useRef(false);
+
     useEffect(() => {
+        if(!mountedRef.current) {
+            mountedRef.current = true
+            console.log("Mounted for the first time!");
+        }
         ref.current?.focus(); 
     }, [])
     return <input type="text" ref={ref}/>
