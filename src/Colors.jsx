82b27a3d53adae6b1a976f51fecb0ff13e5f4ffb
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-12 01:01:55 +0200

    ex55

--- src/Colors.jsx
+++ src/Colors.jsx
@@ -1,3 +1,8 @@
+function Color({ item }) {
+    return <li>{item.name}</li>
+}
+
+
 export function Colors({ items }) {
-    return <ul>{items.map((color) => <li key={color.id}>{color.name}</li>)}</ul>
+    return <ul>{items.map((color) => <Color key={color.id} item={color} />)}</ul>
 }
