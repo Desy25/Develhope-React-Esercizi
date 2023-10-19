c5881f265d2c58ae87ff6a02e2ebb69af71cbce3
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-10-20 01:45:14 +0200

    ex72

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,7 +3,7 @@ import { LanguageContext } from "./LanguageContext";
 
 
 export function Clock() {
-    // Avevo già fatto precedentemente la parte dello useContext
+    // Se viene rimosso il Context.Provider dal component tree, il componente Clock non potrà accedere al valore fornito da LanguageContext e di conseguenza il componente Clock non sarà in grado di visualizzare il messaggio nella lingua corretta.
     const language = useContext(LanguageContext);
 
     const [date, setDate] = useState(new Date())
